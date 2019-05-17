fdf9e4b1f3cf07ff1adada07e3472c057a04ad1e
Peter J De Sousa <petedes@live.com>
2019-05-17 20:37:25 +0100

    More improvements

--- setup_commands.sh
+++ setup_commands.sh
@@ -1,7 +1,7 @@
 ssh-keygen
 sudo apt-get install -y openvpn python2 python2-dev \
         python3 python3-dev python3-pip htop irssi \
-        vim build-essential gitk
+        vim build-essential gitk zsh curl
 mkdir ~/projects/
 cd $_
 git clone git@github.com:VariableDeclared/DebianConfig.git
@@ -16,7 +16,7 @@ echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee 
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update && sudo apt-get install google-cloud-sdk
 gcloud auth login
-
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \\n    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 git clone git@github.com:sjl/badwolf.git
 cd badwolf
